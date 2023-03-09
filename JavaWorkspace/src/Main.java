b1eb94e8d5b5d6edea7d9b07509e175e26ffe1cb
sylee9809 <sylee9809@naver.com>
2023-03-09 09:11:09 +0900

    java update

--- JavaWorkspace/src/Main.java
+++ JavaWorkspace/src/Main.java
@@ -1,56 +1,190 @@
+import javax.swing.text.Style;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-        String a = "hello";
-        String b = "java";
-        String c = "hello";
+//        int a = 10;
+//        String day = "three";
+//        System.out.println(String.format("I eat %d apples. so I was sick for %s days", a, day));
+//        System.out.println(String.format("Error is %d%%.", 98));
+//        System.out.println(String.format("%10s", "hi"));
+//        System.out.println(String.format("%10sjane", "hi"));
+//        System.out.println(String.format("%.4f", 3.131484));
+//        System.out.printf("I eat %d apples\n", 3);
 
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(c));
+////        Scanner sc = new Scanner(System.in);
+////        String input = sc.nextLine();
+////        String[] arr = input.split("\\.");
+//////        System.out.printf("arr의 길이는 %d 입니다.\n", arr.length);
+//////        System.out.println(arr[0]+arr[1]+arr[2]);
+////        System.out.printf("%04d.%02d.%02d",
+////                Integer.parseInt(arr[0]),
+////                Integer.parseInt(arr[1]),
+////                Integer.parseInt(arr[2])
+//        );
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//        String[] arr = input.split("-");
+//        System.out.println(arr[0]+arr[1]);
+//        Integer a = 80;
+//        Integer b = 75;
+//        Integer c = 55;
+//        Double avg = (a.doubleValue() + b.doubleValue() + c.doubleValue()) / 3;
+//        System.out.printf("평균점수는 %.2f 입니다", avg);
 
-        StringBuffer sb = new StringBuffer();
-        sb.append("hello");
-        sb.append(" ");
-        sb.append("jump to java");
-        String result = sb.toString();
-        System.out.println(result);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("국어: ");
+//        Double a = Double.parseDouble(sc.next());
+//        System.out.print("영어: ");
+//        Double b = Double.parseDouble(sc.next());
+//        System.out.print("수학: ");
+//        Double c = Double.parseDouble(sc.next());
+//
+//        Double avg = (a+b+c) / 3;
+//
+//        System.out.printf("평균점수: %.2f", avg);
 
-        int[] odds = {1, 3, 5, 7, 9};
+//        String idcard = "881120-1234567";
+//        String a = "a:b:c:d";
+//        System.out.println("19"+idcard.substring(0,6)+" "+ idcard.substring(7,14));
+//        System.out.println(idcard.charAt(7));
+//        System.out.println(a.replaceAll(":", "#"));
 
-        String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+//        ArrayList pitches = new ArrayList();
+//        pitches.add("139");
+//        pitches.add("129");
+//        pitches.add("38");
+//        System.out.println(pitches.get(1));
 
-        for (int i =0; i < weeks.length; i++) {
-            System.out.println(weeks[i]);
+//        Integer money = 2000;
+//        if (money >= 3000) {
+//            System.out.println("택시");
+//        }else {
+//            System.out.println("걸어가");
+//        }
+//        Integer money = 2000;
+//        boolean hasCard = true;
+//
+//        if (money >= 3000 || hasCard) {
+//            System.out.println("택시");
+//        }else{
+//            System.out.println("걸어");
+//        }
+//        boolean hasCard = true;
+//        ArrayList<String> pocket = new ArrayList<String>();
+//        pocket.add("paper");
+//        pocket.add("handphone");
+//
+//        if (pocket.contains("money")) {
+//            System.out.println("택시");
+//        }else {
+//            if (hasCard){
+//                System.out.println("택시");
+//            }else {
+//                System.out.println("걸어");
+//            }
+//        }
+//        Scanner sc = new Scanner(System.in);
+//
+//        Integer a,b,c;
+//        a= Integer.parseInt(sc.next());
+//        b= Integer.parseInt(sc.next());
+//        c= Integer.parseInt(sc.next());
+//
+//        if (a%2==0){
+//            System.out.printf("%d\n", a);
+//        }
+//        if (b % 2 == 0) {
+//            System.out.printf("%d\n", b);
+//        }
+//        if (c % 2 == 0) {
+//            System.out.printf("%d\n", c);
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        Integer a, b, c;
+//
+//        a = Integer.parseInt(sc.next());
+//        b = Integer.parseInt(sc.next());
+//        c = Integer.parseInt(sc.next());
+//
+//        if (a % 2 == 0) {
+//            System.out.println("even");
+//        }else{
+//            System.out.println("odd");
+//        }
+//        if (b % 2 == 0) {
+//            System.out.println("even");
+//        }else{
+//            System.out.println("odd");
+//        }
+//        if (c % 2 == 0) {
+//            System.out.println("even");
+//        }else{
+//            System.out.println("odd");
+//        }
+
+
+//        String[] numbers = {"one", "two", "three"};
+//        for(int i =0; i<numbers.length; i++){
+//            System.out.println(numbers[i]);
+//        }
+
+
+//        int[] marks = {90,12,14,15,16};
+//        for(int i =0; i<marks.length; i++){
+//            if(marks[i] >=60){
+//                System.out.println((i+1)+"번 학생은 합격입니다");
+//            }else{
+//                System.out.println((i+1)+"번 학생은 불합격입니다.");
+//            }
+//        }
+
+//        for(int i=2; i<10; i++){
+//            for(int j=1; j<10; j++){
+//                System.out.print(i*j+" ");
+//            }
+//            System.out.println("");
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        Integer a, b;
+//        a = Integer.parseInt(sc.next());
+//        b = Integer.parseInt(sc.next());
+//
+//        Integer big = max(a,b);
+//        Integer small = min(a,b);
+//
+//        for(int i=small; i<=big; i++) {
+//            System.out.print(i+" ");
+//
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        Integer a = Integer.parseInt(sc.next());
+//
+//        for(int i=1; i<10; i++){
+//            System.out.println("*".repeat(i*a));
+//        }
+
+        Scanner sc = new Scanner(System.in);
+
+        Integer n = 0;
+        n = Integer.parseInt(sc.nextLine());
+        String[] arr = sc.nextLine().split(" ");
+        sc.close();
+
+        for (int i =0; i < n; i++){
+            System.out.println(arr[i]);
         }
-        ArrayList pitches = new ArrayList();
-        pitches.add("138");
-        pitches.add(138);
-        pitches.add("142");
-        for (int i = 0; i < pitches.size();i++) {
-            System.out.println(pitches.get(i));
-        }
-
-        ArrayList pitches2 = new ArrayList();
-        pitches2.add("139");
-        pitches2.add("123");
-
-        String one = (String) pitches2.get(0);
-        String two = (String) pitches2.get(1);
 
 
-        ArrayList<String> pitches3 = new ArrayList<>(Arrays.asList("138",
-                "129", "142"));
-        String result2 = "";
-        for (int i = 0; i < pitches.size(); i++) {
-            result2 += pitches.get(i);
-            result2 += ",";
-        }
-        result2 = result2.substring(0, result2.length() - 1);
-        System.out.println(result2);
+
 
     }
 }
