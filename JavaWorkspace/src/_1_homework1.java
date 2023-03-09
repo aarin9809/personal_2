bdf16c6947478932080f99d7f17c13d2da7a7412
sylee9809 <sylee9809@naver.com>
2023-03-09 09:40:18 +0900

    java update

---
+++ JavaWorkspace/src/_1_homework1.java
@@ -0,0 +1,13 @@
+public class _1_homework1 {
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 75;
+        int c = 55;
+        int avg;
+
+        avg = (a + b + c) / 3;
+
+        System.out.println(avg);
+
+    }
+}
