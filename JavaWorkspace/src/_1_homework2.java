794e5cc019212f1dbbb2ff5646ef0ef726dcc466
sylee9809 <sylee9809@naver.com>
2023-03-09 09:41:11 +0900

    java update

---
+++ JavaWorkspace/src/_1_homework2.java
@@ -0,0 +1,12 @@
+public class _1_homework2 {
+    public static void main(String[] args) {
+        int a = 13;
+
+
+        if (a % 2 == 0) {
+            System.out.println("짝수");
+        } else{
+            System.out.println("홀수");
+        }
+    }
+}
