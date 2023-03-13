2702fb850048ee9a268a73d3cff2b1b3fc3865bf
sylee9809 <sylee9809@naver.com>
2023-03-13 10:35:45 +0900

    java update

---
+++ JavaWorkspace/src/_1_homework7.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _1_homework7 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>(Arrays.asList("Life", "is",
+                "too", "short"));
+        System.out.println(myList);
+
+    }
+}
