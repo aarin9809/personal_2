6843a47baed6d00747aaab956a95257d67d45a25
sylee9809 <sylee9809@naver.com>
2023-03-27 11:34:00 +0900

    java update

--- JavaWorkspace/src/CodeUp_1092.java
+++ JavaWorkspace/src/CodeUp_1092.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CodeUp_1092 {
+import java.util.*;
+public class CodeUp_1092 {
+    public static void main(String[] args) {
+        //1092
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long c = sc.nextInt();
+
+        int day = 1;
+
+        while (day % a != 0 || day % b != 0 || day % c != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
 }
