7ccc15c266ed4484c40e586a00563b74d1e4e2f4
sylee9809 <sylee9809@naver.com>
2023-03-27 09:59:13 +0900

    java update

--- JavaWorkspace/src/CodeUp_1038.java
+++ JavaWorkspace/src/CodeUp_1038.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class CodeUp_1038 {
+import java.util.Scanner;
+
+public class CodeUp_1038 {
+    public static void main(String[] args) {
+//1038
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.nextLine().charAt(0);
+        System.out.printf("%c",a+1);
+    }
 }
