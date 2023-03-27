f4eb0aedd63b7c7a5e0a0957a10af33eee21f1e3
sylee9809 <sylee9809@naver.com>
2023-03-27 09:59:39 +0900

    java update

--- JavaWorkspace/src/CodeUp_1085.java
+++ JavaWorkspace/src/CodeUp_1085.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CodeUp_1085 {
+import java.util.Scanner;
+
+public class CodeUp_1085 {
+    public static void main(String[] args) {
+//1085
+        Scanner sc = new Scanner(System.in);
+        double h = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+        double s = sc.nextInt();
+
+        double result = (h*b*c*s) /8 /1024/1024;
+        System.out.printf("%.1f MB", result);
+    }
 }
