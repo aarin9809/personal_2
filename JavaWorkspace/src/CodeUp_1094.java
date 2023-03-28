e2ce9c304198b1d204b5e41c8a76c1f418c6ef1a
sylee9809 <sylee9809@naver.com>
2023-03-28 14:46:37 +0900

    java update

--- JavaWorkspace/src/CodeUp_1094.java
+++ JavaWorkspace/src/CodeUp_1094.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CodeUp_1094 {
+import java.util.Scanner;
+
+public class CodeUp_1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i =0; i<n; i++) {
+            int num = sc.nextInt();
+            arr[i] = num;
+        }
+
+        for (int i=n-1; i>=0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+
+    }
 }
