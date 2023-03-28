ff0ebc6f08c88924a2d82cb320c26e1ea2cafb42
sylee9809 <sylee9809@naver.com>
2023-03-28 16:57:05 +0900

    java update

--- JavaWorkspace/src/CodeUp_1462.java
+++ JavaWorkspace/src/CodeUp_1462.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class CodeUp_1462 {
+import java.util.Scanner;
+
+public class CodeUp_1462 {
+    public static void main(String[] args) {
+        //1462
+        Scanner sc = new Scanner(System.in);
+
+        //2차원 배열 생성
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        int cnt = 1;
+
+        //2차원 배열 초기화
+        for (int y =0; y<n; y++) {
+            for(int x =0; x<n; x++){
+                arr[x][y] = cnt;            //
+                cnt++;
+            }
+        }
+
+        //2차원 배열 출력
+        for (int y=0; y<n; y++){
+            for (int x=0; x<n; x++){
+                System.out.printf("%d ", arr[y][x]);
+            }
+            System.out.println("");
+        }
+
+    }
 }
