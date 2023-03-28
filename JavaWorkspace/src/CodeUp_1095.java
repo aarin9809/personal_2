378ba31b62650dbb4761cb82300b8a1363f428f5
sylee9809 <sylee9809@naver.com>
2023-03-28 14:47:00 +0900

    java update

--- JavaWorkspace/src/CodeUp_1095.java
+++ JavaWorkspace/src/CodeUp_1095.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CodeUp_1095 {
+import java.util.Scanner;
+
+public class CodeUp_1095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int min = 25;
+        for (int i = 0; i<n; i++) {
+            int num = sc.nextInt();
+            min = num < min ? num : min;
+        }
+        System.out.println(min);
+    }
 }
