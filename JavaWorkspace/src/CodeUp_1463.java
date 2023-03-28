1b010d02afcce74ca50c0bbe01590bbc31a51db9
sylee9809 <sylee9809@naver.com>
2023-03-28 16:57:35 +0900

    java update

--- JavaWorkspace/src/CodeUp_1463.java
+++ JavaWorkspace/src/CodeUp_1463.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CodeUp_1463 {
+import java.util.Scanner;
+
+public class CodeUp_1463 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int cnt = 1;
+
+
+        for (int x=0; x<n; x++) {
+            for (int y=n-1; y>=0; y--) {
+                arr[y][x] = cnt;
+                cnt++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println("");
+        }
+
+
+
+    }
 }
