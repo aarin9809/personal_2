3ffe9d07deb9e643e487369e3af09c84f004ef95
sylee9809 <sylee9809@naver.com>
2023-03-28 16:58:45 +0900

    java update

--- JavaWorkspace/src/CodeUp_1468.java
+++ JavaWorkspace/src/CodeUp_1468.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class CodeUp_1468 {
+import java.util.Scanner;
+
+public class CodeUp_1468 {
+    public static void main(String[] args) {
+        //4x4 2차원 배열
+        /*1234
+          8765
+          9101112
+          ...
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        int cnt = 1;
+
+        for (int i=0; i<n; i++){
+            if (i % 2 != 0) {
+                for (int j=n-1; j>=0; j--) {
+                    arr[i][j] = cnt;
+                    cnt++;
+                }
+            }
+            else {
+                for (int j=0; j<n; j++) {
+                    arr[i][j] = cnt;
+                    cnt++;
+                }
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println("");
+        }
+
+
+
+    }
 }
