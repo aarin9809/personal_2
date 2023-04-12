e50961ac1abbedec6bdef3594e3b71d38530bb8a
sylee9809 <sylee9809@naver.com>
2023-04-12 15:46:09 +0900

    백준 2884 java update

---
+++ JavaWorkspace/src/bj_2884.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bj_2884 {
+    public static void main(String[] args) {
+        // 45분 일찍 일어나는 알람 문제
+        // 24시간 표현
+        // 입력은 공백을 기준으로 구분되어 입력.
+        // 출력시 불필요한 0은 사용하지 않음.
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (b < 45) {
+            a--;
+            b = 60 - (45 -b);
+            if (a < 0) {
+                a = 23;
+            }
+            System.out.println(a + " " + b);
+        }else {
+            System.out.println(a + " " + (b - 45));
+        }
+    }
+}
