fb4c0c9c8e4b6dae7774e891ab2196e19fd5bb37
aarin9809 <sylee9809@naver.com>
2023-04-28 17:30:47 +0900

    repush2

--- JavaWorkspace/src/CarEx.java
+++ JavaWorkspace/src/CarEx.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CarEx {
+import java.util.Scanner;
+
+public class CarEx {
+    Scanner sc = new Scanner(System.in);
+    int gasVal = 0;
+
+    Car mycar = new Car();
+    mycar.setGas(5);
+
+    boolean gasStatus = mycar.isLeftGas();
+
+    while(true) {
+        if (gasStatus) {
+            System.out.println("출발합니다.");
+            mycar.run()
+        }
+    }
+
+
 }
