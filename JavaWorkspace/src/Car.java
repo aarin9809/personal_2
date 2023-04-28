97e8419769782321103002151eb6931a47d715df
aarin9809 <sylee9809@naver.com>
2023-04-28 17:29:53 +0900

    repush

--- JavaWorkspace/src/Car.java
+++ JavaWorkspace/src/Car.java
@@ -1,2 +1,32 @@
 public class Car {
+    // 인스턴스 변수 생성
+    int gas;
+
+    // 연료 넣는 기능
+    void setGas(int gas) {
+        this.gas = gas;
+    }
+
+    //연료가 있는 지 없는 지 판별
+    boolean isLeftGas() {
+        if (gas == 0) {
+            System.out.println("멈춥니다.");
+            return false;
+        }else {
+            System.out.println("연료가 있습니다.");
+            return true;
+        }
+    }
+
+    void run() {
+        while(true) {
+            if (gas > 0) {
+                System.out.println(gas);
+                gas--;
+            }else {
+                System.out.println("멈춥니다.");
+                break;
+            }
+        }
+    }
 }
